8fdd1eb5a19677d03bd0d7933ca69604eeb328cc
AbdulRehman <153705975+AbdulRehman817@users.noreply.github.com>
2024-02-29 23:39:14 +0500

    alerts

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+Assignment 1:Write a script to greet your website visitor using JS alert box.*/
+var abc="Hello. Welcome to our website"
+alert(abc)
+
+var abd="Error!Please enter a valid password"
+alert(abd)
+
+var abd="Welcome to JS land \n Happy coding"
+alert(abd)
+
